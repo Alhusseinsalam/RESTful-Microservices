2c8bbfdaeee6d2626ad28dc0d6cb703dc2026062
alhusseinsalam <alhussein.salam@gmail.com>
2021-02-14 21:29:14 +0300

    added another endpoint user

--- src/main/java/dev.husein/User.java
+++ src/main/java/dev.husein/User.java
@@ -1,19 +1,24 @@
 package dev.husein;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-@Path("hello")
-public class HelloWorldResource {
+@Path("user")
+public class User {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response helloWorld() {
-        Map<String, String> response = Collections.singletonMap("message", "Hello World");
+        Map<String, String> response = new HashMap<>();
+        response.put("Name", "Husein");
+        response.put("Age", "25");
+        response.put("Weight", "100");
         return Response.ok(response).build();
     }
 }
